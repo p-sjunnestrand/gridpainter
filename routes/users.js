f520461b73e9ac0d48c00749fa1c3f9229267be2
Emma Hammar <emmahammar@emmas-macbook-2.lan>
2021-05-17 15:45:55 +0200

    Adds startpage

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+let htmlHead = 
+  `<link rel="stylesheet" href="/stylesheets/style.css" type="text/css">
+  <title>GridPainter av Grupp 8</title>`;
+
+let htmlHeader = 
+  `<header><h1>GridPainter</h1></header>`; 
+
+let htmlFooter = 
+  `<footer>Av: Grupp 8</footer>`; 
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  let printStartPage = htmlHead + htmlHeader +
+    `<main>
+      <div id="welcomeContainer">Skapa bilder tillsammans med dina vänner!</div>
+
+      <div id="loginContainer">
+        <input id="inputUserName" placeholder="Skriv ett användarnamn..."></input>
+        <a id="startGameBtn" href="">Starta spelet</a>
+      </div>
+
+      <div id="galleryContainer">
+        <a id="galleryBtn" href="">Visa bildgalleri</a>
+      </div>
+    </main>` + htmlFooter; 
+
+  res.send(printStartPage);
 });
 
 module.exports = router;
