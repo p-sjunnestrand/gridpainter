efae2a5c9649230ecc84db695e2d74cd6faad311
Emma Hammar <emmahammar@emmas-macbook-2.lan>
2021-05-19 14:05:40 +0200

    prints grid, adds sass, cathces clickevent in grid

---
+++ public/modules/print-board.mjs
@@ -0,0 +1,40 @@
+export function printBoard() {
+
+    let root = document.getElementById("root"); 
+    let board = `
+        <main>
+            <div id="gameBoardContainer">
+                <h2>Welcome Janne</h2>
+                <section>
+                    <h3>Your Color</h3>
+                    <div>
+                        <div id="color1"></div>
+                        <div id="color2"></div>
+                        <div id="color3"></div>
+                        <div id="color4"></div>
+                    </div>
+                    <div id="gridContainer">`;
+
+    for (let r=1; r<16; r++) {
+
+        for (let c=1; c<16; c++) {
+            board += `<div id=y${r}x${c} class=pixels></div>`;
+        }
+    };
+
+    board += 
+                    `</div>
+
+                </section>
+
+            </div>
+        </main>`;
+        
+    root.innerHTML = board;
+
+    let gridContainer = document.getElementById("gridContainer");
+    gridContainer.addEventListener("click", function(e) {
+        console.log("klickade id:et är: ", e.path[0].id);
+    })
+
+}
