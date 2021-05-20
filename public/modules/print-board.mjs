0349dad7eb8d5347d034223122084a299800dcf5
Igor Milosevic <igor.milosevic1988@gmail.com>
2021-05-20 09:19:18 +0200

    small changes

--- public/modules/print-board.mjs
+++ public/modules/print-board.mjs
@@ -1,8 +1,8 @@
 export function printBoard() {
 
-    let root = document.getElementById("root"); 
+    let root = document.getElementById("root");
     let board = `
-        <main>
+        <div>
             <div id="gameBoardContainer">
                 <h2>Welcome Janne</h2>
                 <section>
@@ -15,25 +15,25 @@ export function printBoard() {
                     </div>
                     <div id="gridContainer">`;
 
-    for (let r=1; r<16; r++) {
+    for (let r = 1; r < 16; r++) {
 
-        for (let c=1; c<16; c++) {
+        for (let c = 1; c < 16; c++) {
             board += `<div id=y${r}x${c} class=pixels></div>`;
         }
     };
 
-    board += 
-                    `</div>
+    board +=
+        `</div>
 
                 </section>
 
             </div>
-        </main>`;
-        
+        </div>`;
+
     root.innerHTML = board;
 
     let gridContainer = document.getElementById("gridContainer");
-    gridContainer.addEventListener("click", function(e) {
+    gridContainer.addEventListener("click", function (e) {
         console.log("klickade id:et är: ", e.path[0].id);
     })
 
