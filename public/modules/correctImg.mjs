68664358cc3080269bed862a4ba6df146ab87e4a
Petter Sjunnestrand <p.sjunnestrand@gmail.com>
2021-05-27 11:07:52 +0200

    fixed merge

--- public/modules/correctImg.mjs
+++ public/modules/correctImg.mjs
@@ -1,7 +1,6 @@
 // import { Socket } from "./socket.io";
 
 
-<<<<<<< HEAD
 export function correctImg(stopTimeRoute) {
   fetch('http://localhost:3000/correctImg')
   // console.log("correctMsgContainer", correctMsgContainer);
@@ -30,34 +29,6 @@ export function correctImg(stopTimeRoute) {
 
   // socket.emit("printScore", {"score": scorePercentage});
   
-=======
-export function correctImg(stopTimeRoute, correctMsgContainer) {
-  console.log("correctMsgContainer", correctMsgContainer);
-  const savedImg = JSON.parse(localStorage.getItem('gridColors'));
-  console.log(savedImg);
-
-  const facitImg = JSON.parse(localStorage.getItem("facitGrid"));
-  console.log(facitImg);
-
-  const total = 225;
-  let sum = 0;
-
-  function compareArrays(savedImg, facitImg) {
-    for(let i = 0; i<savedImg.length; i++){
-      if(savedImg[i].color !=null && facitImg[i].color === savedImg[i].color) {
-        sum++;
-      }
-    }
-  }
-  compareArrays(savedImg, facitImg);
-  console.log("sum", sum);
-
-  //avrunda uppåt:
-  let scorePercentage = Math.floor(sum*100/total);
-  console.log("scorePercentage rounded up", scorePercentage);
-
-  socket.emit("printScore", {"score": scorePercentage});
->>>>>>> main
   
 
 
